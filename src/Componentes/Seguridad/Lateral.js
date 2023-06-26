f1ac683d5d7b3c4992438fc662c0990c9a6f4b77
Robinson Aguilar <robin019@hotmail.com>
2023-06-26 18:53:35 -0500

    se agrego el enrutaiento

--- src/Componentes/Seguridad/Lateral.js
+++ src/Componentes/Seguridad/Lateral.js
@@ -1,48 +1,88 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Lateral = () => {
   return (
     <aside className="lateral">
-        <div className='box-button'>
-            <h3>INVENTARIO</h3>
-            <ul>
-                <li>
-                    <a href='/#'>Agregar</a>
-                </li>
-                <li>
-                    <a href='/#'>Modificar</a>
-                </li>
-                <li>
-                    <a href='/#'>Eliminar</a>
-                </li>
-                <li>
-                    <a href='/#'>Consultar</a>
-                </li>
-            </ul>
-        </div>
-        <div className='box-button'>
-                <h3>ESTADISTICAS</h3>
-                <ul>
-                <li>
-                    <a href='/#'>Rotacion</a>
-                </li>
-                <li>
-                    <a href='/#'>Ganancias</a>
-                </li>
-            </ul>
-        </div>
-        <div className='box-button'>
-            <h3>VENTAS</h3>
-            <ul>
-                <li>
-                    <a href='/#'>Generar</a>
-                </li>
-                <li>
-                    <a href='/#'>Visualizar</a>
-                </li>
-            </ul>
-        </div>
-        
+      <div className="box-button">
+        <h3>INVENTARIO</h3>
+        <ul>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Agregar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Modificar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Eliminar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Consultar
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="box-button">
+        <h3>ESTADISTICAS</h3>
+        <ul>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Rotacion
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Ganancias
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="box-button">
+        <h3>VENTAS</h3>
+        <ul>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Generar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/inicio"}
+              className={({ isActive }) => (isActive === true ? "active" : "")}
+            >
+              Visualizar
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </aside>
-  )
-}
+  );
+};
