36efd18930eb09cbe990fd97e93d84f1cfbe9f6a
Robinson Aguilar <robin019@hotmail.com>
2023-06-27 12:11:29 -0500

    se añadio primeReact

--- src/Componentes/AgregarItem.jsx
+++ src/Componentes/AgregarItem.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
 
 export const AgregarItem = () => {
   return (
-    <div className='content'>
-      <h1>Agregar</h1>
+    <div>
+      <div className="p-inputgroup">
+    <span className="p-inputgroup-addon">
+        <i className="pi pi-user"></i>
+    </span>
+    <InputText placeholder="Username" />
+</div>
+
+<div className="p-inputgroup">
+    <span className="p-inputgroup-addon">$</span>
+    <InputNumber placeholder="Price" />
+    <span className="p-inputgroup-addon">.00</span>
+</div>
+
+<div className="p-inputgroup">
+    <span className="p-inputgroup-addon">www</span>
+    <InputText placeholder="Website" />
+</div>
     </div>
   )
 }
