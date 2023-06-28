5fcfdc0602f3fea1b6e6f407389ada828989122a
Robinson Aguilar <robin019@hotmail.com>
2023-06-28 17:28:13 -0500

    se añadieron dos hojas de estilo nuevas

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './robin.css';
+import './lau.css'
 import 'primeicons/primeicons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
