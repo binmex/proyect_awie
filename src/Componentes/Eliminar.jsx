7fcad43cee8feb6324c054c40986a5d39c88a054
Lauragranados2021 <laura.granados05@uptc.edu.co>
2023-07-01 12:43:31 -0500

    modificar listo eliminar listo
    faltan iconos

--- src/Componentes/Eliminar.jsx
+++ src/Componentes/Eliminar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Button } from "primereact/button";
+
 
 export const Eliminar = () => {
+ 
   return (
     <div className='content'>
-        <h1>Eliminar</h1>
+        <div className='Modificar'>
+          <h3>ELIMINAR</h3>
+         < div className='boxBorrar'>
+         <label>ID: </label>
+         <input type="number" id="eliminar" name="eliminar" />
+         <button  type="submit">Seleccionar</button>
+          </div> 
+          <div className="boxInput">
+        <div className="inputAdd">
+            <label>ID: </label>
+            
+          </div>
+          <div className="inputAdd">
+            <label>Nombre:</label>
+    
+        
+          </div>
+          
+          <div className="inputAdd">
+            <label> Compra: </label>
+            
+          </div>
+          <div className="inputAdd">
+            <label> Venta: </label>
+            
+          </div>
+          <div className="inputAdd">
+            <label>Cantidad:</label>
+            
+          </div>
+          <div className="inputAdd">
+            <label>Ingreso:</label>
+            
+          </div>
+          
+          <div className="inputAdd2 button">
+            <Button label="Borrar" severity="danger" />
+          </div>
+        </div>
+          
+
+        </div>
     </div>
   )
 }
--- src/Componentes/ModItem.jsx
+++ src/Componentes/ModItem.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+import { Calendar } from "primereact/calendar";
+
 
 export const ModItem = () => {
+  const [date, setDate] = useState(null);
   return (
-    <div className='content'>
-      <h1>Modificar</h1>
+    <div className="content">
+      <div className="Modificar">
+        <h3>Modificar</h3>
+        <div className="boxInput">
+        <div className="inputAdd">
+            <label>ID: </label>
+            <InputNumber inputId="integeronly" />
+          </div>
+          <div className="inputAdd">
+            <label>Nombre</label>
+            <InputText
+              type="text"
+              className="p-inputtext-sm"
+            />
+          </div>
+          
+          <div className="inputAdd">
+            <label>Compra: </label>
+            <InputNumber inputId="integeronly" />
+          </div>
+          <div className="inputAdd">
+            <label>Venta: </label>
+            <InputNumber inputId="integeronly" />
+          </div>
+          <div className="inputAdd">
+            <label>Cantidad</label>
+            <InputNumber inputId="integeronly" />
+          </div>
+          <div className="inputAdd">
+            <label>Ingreso </label>
+            <Calendar
+              value={date}
+              onChange={(e) => setDate(e.value)}
+              showIcon
+            />
+          </div>
+          <div className="inputAdd2 button">
+            <Button label="Limpiar" severity="warning" />
+            <Button label="Actualizar" severity="info" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
